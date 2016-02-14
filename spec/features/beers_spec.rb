8ed1007c646bcf77ee9ff61d518fd2decfbd3dc0
Eetu Sepp„l„ <eetu.seppala@helsinki.fi>
2016-02-14 19:40:42 +0200

    viikko 4 paketissa

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -5,8 +5,14 @@ include Helpers
 describe "New beer page" do
 
 let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+let!(:user) { FactoryGirl.create :user }
 
   it "can create a new beer" do
+    visit signin_path
+    fill_in('username', with:'Pekka')
+    fill_in('password', with:'Foobar1')
+    click_button('Log in')
+    
     visit new_beer_path
     fill_in('beer[name]', with:'Kalja')
     select('Koff', from:'beer[brewery_id]')
@@ -18,6 +24,11 @@ let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
   end
 
   it "doesn't save a beer with invalid name" do
+    visit signin_path
+    fill_in('username', with:'Pekka')
+    fill_in('password', with:'Foobar1')
+    click_button('Log in')
+
     visit new_beer_path
 
     select('Koff', from:'beer[brewery_id]')
